553af522c03cb29a41f1d3a659bfc722679c4964
Greg Pechiro <gregpechiro@yahoo.com>
2013-11-21 16:52:09 -0500

    RC2

--- serv.go
+++ serv.go
@@ -1,18 +1,9 @@
 package data
 
 import (
-	"encoding/json"
-	"io"
 	"log"
 	"net"
-	"time"
 )
-
-type M struct {
-	Cmd string      `json:"cmd,omitempty"`
-	Key string      `json:"key,omitempty"`
-	Val interface{} `json:"val,omitempty"`
-}
 
 type Server struct {
 	st *Store
@@ -31,29 +22,6 @@ func (self *Server) Listen(port string) {
 	}
 	for {
 		conn, _ := ln.Accept()
-		go self.Handle(conn)
-	}
-}
-
-func (self *Server) Handle(conn net.Conn) {
-	enc, dec := json.NewEncoder(conn), json.NewDecoder(conn)
-	for {
-		var m M
-		if err := dec.Decode(&m); err == io.EOF {
-			break
-		} else if err != nil {
-			conn.Close()
-			return
-		} else {
-			conn.SetDeadline(time.Now().Add(time.Second * 300))
-		}
-		switch m.Cmd {
-		case "set":
-			enc.Encode(self.st.Set(&m))
-		case "get":
-			enc.Encode(self.st.Get(&m))
-		default:
-			enc.Encode(M{Val: "false"})
-		}
+		go self.st.Handle(conn)
 	}
 }
--- store.go
+++ store.go
@@ -1,8 +1,18 @@
 package data
 
 import (
+	"encoding/json"
+	"io"
+	"net"
 	"sync"
+	"time"
 )
+
+type M struct {
+	Cmd string      `json:"cmd,omitempty"`
+	Key string      `json:"key,omitempty"`
+	Val interface{} `json:"val,omitempty"`
+}
 
 type Store struct {
 	data map[string]interface{}
@@ -31,4 +41,27 @@ func (self *Store) Get(m *M) M {
 	}
 	self.RUnlock()
 	return M{Val: false}
+}
+
+func (self *Store) Handle(conn net.Conn) {
+	enc, dec := json.NewEncoder(conn), json.NewDecoder(conn)
+	for {
+		var m M
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			conn.Close()
+			return
+		} else {
+			conn.SetDeadline(time.Now().Add(time.Second * 300))
+		}
+		switch m.Cmd {
+		case "set":
+			enc.Encode(self.Set(&m))
+		case "get":
+			enc.Encode(self.Get(&m))
+		default:
+			enc.Encode(M{Val: "false"})
+		}
+	}
 }
